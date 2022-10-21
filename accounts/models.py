f4123df214f1088c97d7f5f54b0923e0fe890804
최준혁 <fx887722@naver.com>
2022-10-21 15:53:07 +0900

    Django 환경설정, 회원가입, 로그인 기능 구현

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# AbstracuUser 상속
+class User(AbstractUser):
+    pass
---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+
+
+
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save() # ModelForm의 save 메서드의 리턴값은 해당 모델의 인스턴스다!
+            auth_login(request, user) # 로그인
+            return redirect('reviews:index')
+    else:   
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'reviews:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
